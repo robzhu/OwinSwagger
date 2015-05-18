3dd7cf7c04b6d90150eaf4e9b8d83c3d9ca0c91c
Robert Zhu <robzhu@gmail.com>
2015-05-17 23:12:56 -0400

    Initial checkin

---
+++ OwinSwagger/BookController.cs
@@ -0,0 +1,47 @@
+﻿using System.Web.Http;
+using System.Web.Http.Description;
+
+namespace OwinSwagger
+{
+    /// <summary>
+    /// Resource representing a book.
+    /// </summary>
+    public class Book
+    {
+        /// <summary>
+        /// The self-link
+        /// </summary>
+        public string Href { get; set; }
+
+        /// <summary>
+        /// The name of the author
+        /// </summary>
+        public string Author { get; set; }
+
+        /// <summary>
+        /// The title of the book
+        /// </summary>
+        public string Title { get; set; }
+    }
+
+    public class BookController : ApiController
+    {
+        /// <summary>
+        /// Retrieves a book with the specified Id.
+        /// </summary>
+        /// <param name="id">The id of the book to retrieve</param>
+        [ResponseType( typeof( Book ) )]
+        public IHttpActionResult Get( string id )
+        {
+            //a real controller would need to take the id and look up the book in the database
+            //let's hack it below to always return a single book
+            var book = new Book
+            {
+                Author = "Stephen King",
+                Title = "Hearts in Atlantis",
+                Href = Request.RequestUri.ToString(),
+            };
+            return Ok( book );
+        }
+    }
+}
---
+++ OwinSwagger/Startup.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Net.Http.Formatting;
+using System.Web.Http;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Serialization;
+using Owin;
+using Swashbuckle.Application;
+
+namespace OwinSwagger
+{
+    public class Startup
+    {
+        public void Configuration( IAppBuilder app )
+        {
+            var config = new HttpConfiguration();
+
+            //Use JSON friendly default settings
+            var defaultSettings = new JsonSerializerSettings
+            {
+                Formatting = Formatting.Indented,
+                ContractResolver = new CamelCasePropertyNamesContractResolver(),
+                Converters = new List<JsonConverter>{ new StringEnumConverter{ CamelCaseText = true }, }
+            };
+            JsonConvert.DefaultSettings = () => { return defaultSettings; };
+
+            //Specify JSON as the default media type
+            config.Formatters.Clear();
+            config.Formatters.Add( new JsonMediaTypeFormatter() );
+            config.Formatters.JsonFormatter.SerializerSettings = defaultSettings;
+
+            //Route all requests to the RootController by default
+            config.Routes.MapHttpRoute( "api", "{controller}/{id}", defaults: new { id = RouteParameter.Optional } );
+            config.MapHttpAttributeRoutes();
+
+            //Tell swagger to generate documentation based on the XML doc file output from msbuild
+            config.EnableSwagger( c =>
+            {
+                c.IncludeXmlComments( "docs.xml" );
+                c.SingleApiVersion( "1.0", "Owin Swashbuckle Demo" );
+            } ).EnableSwaggerUi();
+
+            app.UseWebApi( config );
+        }
+    }
+}
